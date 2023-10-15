96c7a6a412f5409dbbd76e9eaea54500002ec7c5
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2023-10-15 18:10:41 +0200

    Second batch of .NET 8 and C# 12 examples

---
+++ AspNetCore8.ApiIdentityEndpoints/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore8.ApiIdentityEndpoints;
+
+public class AppUser : IdentityUser
+{
+    // Add customisations here later
+}
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+      : base(options)
+    {
+    }
+}
